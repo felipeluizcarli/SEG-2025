423ed7b507d04cfd60cc0642a256cffca70c7f63
FELIPE <felipe_carli@hotmail.com>
2025-10-31 21:55:55 -0300

    31/10

---
+++ beckend/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupabaseModule } from './supabase/supabase.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    SupabaseModule,
+    AuthModule,
+    UserModule
+    
+  ],
+})
+export class AppModule {}
